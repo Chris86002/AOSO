9fc5a90076ae75a1cadc3d84b09444477d6f8772
Chris86002 <141607373+Chris86002@users.noreply.github.com>
2026-09-22 14:52:39 -0700

    Bump native addon for expanded porkchop search

--- plugin/kOS.AddOns.AOSO/Addon.cs
+++ plugin/kOS.AddOns.AOSO/Addon.cs
@@ -23,7 +23,7 @@ namespace kOS.AddOns.AOSO
 
         private void InitializeAosoSuffixes()
         {
-            AddSuffix("VERSION", new Suffix<StringValue>(() => new StringValue("0.2.1")));
+            AddSuffix("VERSION", new Suffix<StringValue>(() => new StringValue("0.3.0")));
             AddSuffix("LAMBERT", new OneArgsSuffix<Lexicon, Lexicon>(Lambert));
             AddSuffix("PORKCHOP", new OneArgsSuffix<Lexicon, Lexicon>(Porkchop));
             AddSuffix("PORKCHOPSTART", new OneArgsSuffix<Lexicon, Lexicon>(PorkchopStart));
